dfdf9b30f64d509b89103da28ebe52530217507a
Daniel Gordon <dgordon8765@gmail.com>
2015-05-04 11:17:43 -0500

    Removed work email

--- src/PokerRunner.java
+++ src/PokerRunner.java
@@ -1,15 +1,5 @@
-/*
- * Copyright 2014 HERE Inc. All rights reserved.
- *
- *     This software is the confidential and proprietary information of HERE Inc.
- *     You shall not disclose such confidential information and shall use it only in
- *     accordance with the terms of the license agreement you entered into with
- *     HERE Inc.
- */
-
-
 /**
- * @author Daniel Gordon <daniel.gordon@here.com>
+ * @author Daniel Gordon 
  */
 public class PokerRunner {
     public static void main (String args[]) {
