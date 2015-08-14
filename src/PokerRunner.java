24d4e9bb6d60903b5a90bf0ad2c63e628496a0a1
Daniel Gordon <dgordon8765@gmail.com>
2015-08-14 12:07:59 -0500

    cleaning up .idea files from a bad commit

--- src/PokerRunner.java
+++ src/PokerRunner.java
@@ -6,9 +6,16 @@ public class PokerRunner {
 
     }
 
-    public String pokerLineReader () {
-
-        return "";
+    /*
+    *
+    * Method to read one line from the input file and return two Hand objects
+    * Read the line, then split it up into two strings to create two hands.
+    * How to split up the line into two strings?
+    *
+     */
+    public Hand[] pokerLineReader () {
+        System.in.read("input.txt");
+        return null;
     }
 
 }
